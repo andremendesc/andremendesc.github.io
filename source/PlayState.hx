fa71a9780fe0bf8414d09afc229b1b87ca6ba47a
André Mendes <andre.mendesc@gmail.com>
2019-11-14 00:38:47 -0300

    Ajusta hitbox para ser menos estrita e 'fixa' a colisão que estava errada, o que nem era bug pois faltou o comando

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -27,97 +27,53 @@ class PlayState extends FlxState
 		add(_bg);
 
 		_map = new FlxOgmoLoader(AssetPaths.level4__oel);
+
 		_mForeground = _map.loadTilemap(AssetPaths.tilemap_packed__png, 16, 16, "foreground");
 		_mForeground.follow();
-		_mForeground.setTileProperties(0, FlxObject.ANY);
-		_mForeground.setTileProperties(1, FlxObject.ANY);
-		_mForeground.setTileProperties(2, FlxObject.ANY);
-		_mForeground.setTileProperties(3, FlxObject.ANY);
-		_mForeground.setTileProperties(4, FlxObject.ANY);
-		_mForeground.setTileProperties(5, FlxObject.ANY);
-		_mForeground.setTileProperties(6, FlxObject.ANY);
-		_mForeground.setTileProperties(8, FlxObject.ANY);
-		_mForeground.setTileProperties(9, FlxObject.ANY);
-		_mForeground.setTileProperties(10, FlxObject.ANY);
-		_mForeground.setTileProperties(11, FlxObject.ANY);
-		_mForeground.setTileProperties(12, FlxObject.ANY);
-		_mForeground.setTileProperties(13, FlxObject.ANY);
-		_mForeground.setTileProperties(14, FlxObject.ANY);
-		_mForeground.setTileProperties(15, FlxObject.ANY);
-		_mForeground.setTileProperties(27, FlxObject.ANY);
-		_mForeground.setTileProperties(28, FlxObject.ANY);
-		_mForeground.setTileProperties(35, FlxObject.ANY);
-		_mForeground.setTileProperties(36, FlxObject.ANY);
-		_mForeground.setTileProperties(37, FlxObject.ANY);
-		_mForeground.setTileProperties(38, FlxObject.ANY);
-		_mForeground.setTileProperties(39, FlxObject.ANY);
-		_mForeground.setTileProperties(40, FlxObject.ANY);
-		_mForeground.setTileProperties(41, FlxObject.ANY);
-		_mForeground.setTileProperties(42, FlxObject.ANY);
-		_mForeground.setTileProperties(62, FlxObject.ANY);
-		_mForeground.setTileProperties(63, FlxObject.ANY);
-		_mForeground.setTileProperties(64, FlxObject.ANY);
-		_mForeground.setTileProperties(65, FlxObject.ANY);
-		_mForeground.setTileProperties(66, FlxObject.ANY);
-		_mForeground.setTileProperties(67, FlxObject.ANY);
-		_mForeground.setTileProperties(68, FlxObject.ANY);
-		_mForeground.setTileProperties(69, FlxObject.ANY);
-		_mForeground.setTileProperties(405, FlxObject.ANY);
-		_mForeground.setTileProperties(406, FlxObject.ANY);
-		_mForeground.setTileProperties(432, FlxObject.ANY);
-		_mForeground.setTileProperties(433, FlxObject.ANY);
-		_mForeground.setTileProperties(434, FlxObject.ANY);
-		_mForeground.setTileProperties(435, FlxObject.ANY);
-		_mForeground.setTileProperties(436, FlxObject.ANY);
-		_mForeground.setTileProperties(437, FlxObject.ANY);
-		_mForeground.setTileProperties(438, FlxObject.ANY);
-		_mForeground.setTileProperties(439, FlxObject.ANY);
-		_mForeground.setTileProperties(440, FlxObject.ANY);
-		_mForeground.setTileProperties(441, FlxObject.ANY);
-		_mForeground.setTileProperties(441, FlxObject.ANY);
-		_mForeground.setTileProperties(460, FlxObject.ANY);
-		_mForeground.setTileProperties(461, FlxObject.ANY);
-		_mForeground.setTileProperties(462, FlxObject.ANY);
-		_mForeground.setTileProperties(463, FlxObject.ANY);
-		_mForeground.setTileProperties(464, FlxObject.ANY);
-		_mForeground.setTileProperties(465, FlxObject.ANY);
-		_mForeground.setTileProperties(466, FlxObject.ANY);
-		_mForeground.setTileProperties(467, FlxObject.ANY);
-		_mForeground.setTileProperties(468, FlxObject.ANY);
+		_mForeground.setTileProperties(0, FlxObject.NONE, null, null, 15);
+		_mForeground.setTileProperties(27, FlxObject.NONE, null, null, 2);
+		_mForeground.setTileProperties(35, FlxObject.NONE, null ,null, 7);
+		_mForeground.setTileProperties(62, FlxObject.NONE, null, null, 7);
+		_mForeground.setTileProperties(405, FlxObject.NONE, null, null, 2);
+		_mForeground.setTileProperties(432, FlxObject.NONE, null, null, 10);
+		_mForeground.setTileProperties(459, FlxObject.NONE, null, null, 9);
 		add(_mForeground);
 
 		_mWalls = _map.loadTilemap(AssetPaths.PathAndObjects__png, 16, 16, "walls");
 		add(_mWalls);
 
+		_player = new Player();
+		_player.scale.set(0.020, 0.020);
+		_player.updateHitbox();
+		_player.scale.set(0.035, 0.035);
+		_map.loadEntities(placeEntities, "entities");
+ 		add(_player);
+
 		_mForeground2 = _map.loadTilemap(AssetPaths.tilemap_packed__png, 16, 16, "foreground2");
 		_mForeground2.follow();
-		_mForeground2.setTileProperties(315, FlxObject.ANY);
-		_mForeground2.setTileProperties(316, FlxObject.ANY);
-		_mForeground2.setTileProperties(317, FlxObject.ANY);
-		_mForeground2.setTileProperties(232, FlxObject.ANY);
+		_mForeground2.setTileProperties(315, FlxObject.NONE);
+		_mForeground2.setTileProperties(316, FlxObject.NONE);
+		_mForeground2.setTileProperties(317, FlxObject.NONE);
+		_mForeground2.setTileProperties(232, FlxObject.NONE);
 		_mForeground2.setTileProperties(259, FlxObject.ANY);
 		_mForeground2.setTileProperties(449, FlxObject.ANY);
 		_mForeground2.setTileProperties(342, FlxObject.ANY);
 		_mForeground2.setTileProperties(343, FlxObject.ANY);
 		_mForeground2.setTileProperties(344, FlxObject.ANY);
 		_mForeground2.setTileProperties(398, FlxObject.ANY);
-		_mForeground2.setTileProperties(164, FlxObject.ANY);
+		_mForeground2.setTileProperties(164, FlxObject.NONE);
+		_mForeground2.setTileProperties(391, FlxObject.NONE);
+		_mForeground2.setTileProperties(425, FlxObject.NONE);
 		_mForeground2.setTileProperties(471, FlxObject.ANY);
 		add(_mForeground2);
 
-		_player = new Player();
-		_player.scale.set(0.035, 0.035);
-		_player.updateHitbox();
-		_map.loadEntities(placeEntities, "entities");
- 		add(_player);
+		_btnPlay = new FlxButton(200, 3, "Voltar", clickVoltar);
+		add(_btnPlay);
 
 		_policia = new Policia(120, 120);
 		_policia.scale.set(1.5, 1.5);
 		_policia.updateHitbox();
  		add(_policia);
-
-		_btnPlay = new FlxButton(200, 3, "Voltar", clickVoltar);
-		add(_btnPlay);
 
 		FlxG.camera.follow(_player, TOPDOWN, 1);
 
@@ -139,6 +95,8 @@ class PlayState extends FlxState
 	{
 		super.update(elapsed);
 		FlxG.collide(_player, _mWalls);
+		FlxG.collide(_player, _mForeground);
+		FlxG.collide(_player, _mForeground2);
 	}
 
 	function clickVoltar():Void
